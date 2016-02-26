641357d106d368b77b944fb0b791c7c45478e362
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-25 21:41:34 -0500

    Added HelloWorld Controller

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.HtmlEncode(
+               "Hello " + name + ". ID: " + ID );
+        }
+    }
+}
